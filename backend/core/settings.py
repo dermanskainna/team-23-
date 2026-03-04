0d6a51fc08f519ca841c2392fbdf17747bea9d85
TarasWRS <119341926+TarasWRS@users.noreply.github.com>
2026-03-04 16:30:26 +0200

    дещо для хосту

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -134,7 +134,7 @@ CHANNEL_LAYERS = {
 }
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
